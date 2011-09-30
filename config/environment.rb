87bf7784f385ed064ac45968749198575a862400
roloenusa <roloenusa@msn.com>
2011-09-29 18:32:25 -0700

    Environment settings changed

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => “papanoel.heroku.com” }
+
 # Initialize the rails application
 SecretSanta::Application.initialize!
